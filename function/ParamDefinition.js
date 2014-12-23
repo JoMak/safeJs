ced4f5ab6c2f516c1e3c1cae7107a713dda2c3ec
Karim Piyar Ali <karim.piyarali@gmail.com>
2014-12-22 19:11:31 -0500

    added 'private' fields and other methods
    removed unecessary use stricts
    defined all setters

--- function/ParamDefinition.js
+++ function/ParamDefinition.js
@@ -1,97 +1,224 @@
 /**
  * File: functionSafe.js
  */
-
-
-
 (function() {
-  var ParamDefinition = function(settings) {
-    "use strict";
+  "use strict";
 
+  var ParamDefinition = function(settings) {
+    this.addProperties(settings);
   };
 
   Object.defineProperties(ParamDefinition.prototype, {
+    /**
+     * Properties
+     */
+
+    /**
+     * [Optional] Allow a parameter to be null
+     * Defaults to 'false'
+     */
     'allowNull': {
       configurable: false,
       enumerable: false,
 
       get: function() {
-        "use strict";
-
-        return this.allowNull;
+        return this._allowNull;
       },
 
       set: function(isAllowNull) {
-        "use strict";
-
         if (typeof(isAllowNull) !== 'boolean') {
           throw new TypeError('Property "allowNull" must be of type: "boolean"');
         }
 
-        this.allowNull = isAllowNull;
+        this._allowNull = isAllowNull;
       }
     },
 
+    /**
+     * [Optional] Allow a parameter to be undefined
+     * Defaults to 'false'
+     */
     'allowUndefined': {
       configurable: false,
       enumerable: false,
 
       get: function() {
-        "use strict";
-
-        return this.allowUndefined;
+        return this._allowUndefined;
       },
 
       set: function(isAllowUndefined) {
-        "use strict";
-
         if (typeof(isAllowUndefined) !== 'boolean') {
           throw new TypeError('Property "allowUndefined" must be of type: "boolean"');
         }
 
-        this.allowUndefined = isAllowUndefined;
+        this._allowUndefined = isAllowUndefined;
       }
     },
 
+    /**
+     * [Optional] Allow a parameter to be empty
+     * For string this will be an empty string ('') check,
+     * For Arrays this will check the 'length' property
+     * The property is not checked for any other type
+     * Defaults to 'true'
+     */
     'allowEmpty': {
       configurable: false,
       enumerable: false,
 
       get: function() {
-        "use strict";
-
-        return this.allowEmpty;
+        return this._allowEmpty;
       },
 
       set: function(isAllowEmpty) {
-        "use strict";
-
         if (typeof(isAllowEmpty) !== 'boolean') {
           throw new TypeError('Property "allowEmpty" must be of type: "boolean"');
         }
-        this.allowEmpty = isAllowEmpty;
+        this._allowEmpty = isAllowEmpty;
       }
     },
 
-    'paramType': {
+    /**
+     * [Optional] Valid types the parameter is allowed to be.
+     * Defaults to 'object'
+     */
+    'paramTypes': {
       configurable: false,
       enumerable: false,
 
       get: function() {
-        "use strict";
-
-        return this.paramType;
+        return this._types;
       },
 
-      set: function(type) {
-        "use strict";
-
-        if (typeof(type) !== 'string' || typeof(type) !== 'object') {
-          throw new TypeError('Property "type" must be of type: "TypeDefinition"');
+      set: function(types) {
+        if (!(types instanceof Array)) {
+          types = [types];
         }
-        this.paramType = type;
+
+        try {
+          types.forEach(function(type) {
+            if (typeof(type) !== 'string' || typeof(type) !== 'object') {
+              throw new TypeError('Property "paramTypes" must be of type: "TypeDefinition"');
+            }
+          });
+        } catch (err) {
+          console.error('Could not set "paramType":', types);
+          throw err;
+        }
+
+        this._types = types;
       }
+    },
+
+    /**
+     * [Optional] The position of a parameter in the method.
+     */
+    'pos': {
+      configurable: false,
+      enumerable: false,
+
+      get: function() {
+        return this._pos;
+      },
+
+      set: function(pos) {
+        if (typeof(pos) !== 'number') {
+          throw new TypeError('Property "pos" must be of type: "number"');
+        }
+
+        this._pos = pos;
+      }
+    },
+
+    /**
+     * Fields (because javascript -_-)
+     */
+
+    '_allowNull': {
+      configurable: false,
+      enumerable: false,
+      writable: true,
+      value: false
+    },
+
+    '_allowEmpty': {
+      configurable: false,
+      enumerable: false,
+      writable: true,
+      value: true
+    },
+
+    '_allowUndefined': {
+      configurable: false,
+      enumerable: false,
+      writable: true,
+      value: false
+    },
+
+    '_types': {
+      configurable: false,
+      enumerable: false,
+      writable: true,
+      value: ['object']
+    },
+
+    '_pos': {
+      configurable: false,
+      enumerable: false,
+      writable: true,
+      value: NaN
+    },
+
+    /**
+     * Methods
+     */
+    'addProperties': {
+      configurable: false,
+      enumerable: false
+    },
+
+    'addProperty': {
+      configurable: false,
+      enumerable: false
+    },
+
+    'setDefaults': {
+      configurable: false,
+      enumerable: false
     }
   });
-  
+
+  /**
+   * Add all properties/settings for a ParamDefition in batch
+   * @param {Object | string | Array} settings
+   */
+  ParamDefinition.prototype.addProperties = function ParamDefinition_addProperties(settings) {
+    if (typeof(settings) === 'object') {
+      this.addProperty('allowNull', settings.allowNull);
+      this.addProperty('allowEmpty', settings.allowEmpty);
+      this.addProperty('allowUndefined', settings.allowUndefined);
+      this.addProperty('types', settings.types);
+      this.addProperty('pos', settings.pos);
+
+    } else if ((typeof(settings) === 'string') || (settings instanceof Array)) {
+      this.addProperty('types', settings);
+    }
+  };
+
+  /**
+   * Simple undefined/null checker before adding a property
+   * @param {string} key A valid ParamDefintion property (this will NOT be checked for validity)
+   * @param {*} value Property value - this gets checked for undefined and null
+   */
+  ParamDefinition.prototype.addProperty = function ParamDefinition_addProperty(key, value) {
+    if (typeof(value) !== 'undefined' && value !== null) {
+      try {
+        this[key] = value;
+      } catch (err) {
+        throw ('Could not add key: "' + key + '": ' + err.toString());
+      }
+    }
+  };
+
+  window.ParamDefinition = ParamDefinition;
 })();
