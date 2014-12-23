c2271758bd3b64899142e59c1b32d0dba0695639
Karim Piyar Ali <karim.piyarali@gmail.com>
2014-12-23 02:02:16 -0500

    finished up ParamDefintion

--- function/ParamDefinition.js
+++ function/ParamDefinition.js
@@ -25,12 +25,12 @@
         return this._allowNull;
       },
 
-      set: function(isAllowNull) {
-        if (typeof(isAllowNull) !== 'boolean') {
+      set: function(allowNull) {
+        if (!_.isBoolean(allowNull)) {
           throw new TypeError('Property "allowNull" must be of type: "boolean"');
         }
 
-        this._allowNull = isAllowNull;
+        this._allowNull = allowNull;
       }
     },
 
@@ -46,12 +46,12 @@
         return this._allowUndefined;
       },
 
-      set: function(isAllowUndefined) {
-        if (typeof(isAllowUndefined) !== 'boolean') {
+      set: function(allowUndefined) {
+        if (!_.isBoolean(allowUndefined)) {
           throw new TypeError('Property "allowUndefined" must be of type: "boolean"');
         }
 
-        this._allowUndefined = isAllowUndefined;
+        this._allowUndefined = allowUndefined;
       }
     },
 
@@ -70,11 +70,11 @@
         return this._allowEmpty;
       },
 
-      set: function(isAllowEmpty) {
-        if (typeof(isAllowEmpty) !== 'boolean') {
+      set: function(allowEmpty) {
+        if (!_.isBoolean(allowEmpty)) {
           throw new TypeError('Property "allowEmpty" must be of type: "boolean"');
         }
-        this._allowEmpty = isAllowEmpty;
+        this._allowEmpty = allowEmpty;
       }
     },
 
@@ -91,14 +91,19 @@
       },
 
       set: function(types) {
-        if (!(types instanceof Array)) {
+        if (types == null) {
+          throw new TypeError('Property "paramTypes" must be either ' +
+            '(1) An object, (2) A string or (3) an array of a either objects or strings');
+
+        } else if (!_.isArray(types)) {
           types = [types];
         }
 
         try {
           types.forEach(function(type) {
-            if (typeof(type) !== 'string' || typeof(type) !== 'object') {
-              throw new TypeError('Property "paramTypes" must be of type: "TypeDefinition"');
+            if (!_.isString(type) || !_.isObject(type)) {
+              throw new TypeError('Property "paramTypes" must be either ' +
+                '(1) An object, (2) A string or (3) an array of a either objects or strings');
             }
           });
         } catch (err) {
@@ -122,7 +127,7 @@
       },
 
       set: function(pos) {
-        if (typeof(pos) !== 'number') {
+        if (!_.isNumber(pos)) {
           throw new TypeError('Property "pos" must be of type: "number"');
         }
 
@@ -174,17 +179,26 @@
      */
     'addProperties': {
       configurable: false,
-      enumerable: false
+      enumerable: false,
+      writable: true
     },
 
     'addProperty': {
       configurable: false,
-      enumerable: false
+      enumerable: false,
+      writable: true
     },
 
     'setDefaults': {
       configurable: false,
-      enumerable: false
+      enumerable: false,
+      writable: true
+    },
+
+    'isValidType': {
+      configurable: false,
+      enumerable: false,
+      writable: true
     }
   });
 
@@ -193,14 +207,14 @@
    * @param {Object | string | Array} settings
    */
   ParamDefinition.prototype.addProperties = function ParamDefinition_addProperties(settings) {
-    if (typeof(settings) === 'object') {
+    if (_.isObject(settings)) {
       this.addProperty('allowNull', settings.allowNull);
       this.addProperty('allowEmpty', settings.allowEmpty);
       this.addProperty('allowUndefined', settings.allowUndefined);
       this.addProperty('types', settings.types);
       this.addProperty('pos', settings.pos);
 
-    } else if ((typeof(settings) === 'string') || (settings instanceof Array)) {
+    } else if (_.isString(settings) || _.isArray(settings)) {
       this.addProperty('types', settings);
     }
   };
@@ -211,12 +225,85 @@
    * @param {*} value Property value - this gets checked for undefined and null
    */
   ParamDefinition.prototype.addProperty = function ParamDefinition_addProperty(key, value) {
-    if (typeof(value) !== 'undefined' && value !== null) {
+    if (!_.isUndefined(value) && !_.isNull(value)) {
       try {
         this[key] = value;
       } catch (err) {
-        throw ('Could not add key: "' + key + '": ' + err.toString());
+        console.error('Could not add key: "' + key + '": ' + err.toString());
+        // throw ('Could not add key: "' + key + '": ' + err.toString());
       }
+    }
+  };
+
+  /**
+   * Check if the passed in object/value matches the parameter definition
+   * @param {*} value Value or object to check the parameter types and restrictions against
+   * @return {Boolean}
+   */
+  ParamDefinition.prototype.isValidType = function ParamDefinition_isValidType(value) {
+    //check for undefined, null, and empty first
+    if ((!this._allowNull && _.isNull(value)) ||
+      (!this._allowUndefined && _.isUndefined(value)) ||
+      (!this._allowEmpty && _.isEmpty(value))) {
+      return false;
+    }
+
+    //check for types now
+    return this._types.some(function(type) {
+      if (_.isString(type)) {
+        checkStringType(type, value);
+
+      } else {
+        return (value instanceof type);
+      }
+    }, this);
+
+  };
+
+  var checkStringType = function ParamDefinition_checkStringType(type, value) {
+    switch (type.toLowerCase()) {
+
+      case 'element':
+      case 'el':
+        return _.isElement(value);
+
+      case 'array':
+        return _.isArray(value);
+
+      case 'object':
+      case 'obj':
+        return _.isObject(value);
+
+      case 'arguments':
+      case 'argument':
+      case 'args':
+      case 'arg':
+        return _.isArguments(value);
+
+      case 'function':
+        return _.isFunction(value);
+
+      case 'string':
+      case 'str':
+        return _.isString(value);
+
+      case 'number':
+      case 'int':
+        return _.isNumber(value);
+
+      case 'boolean':
+      case 'bool':
+        return _.isBoolean(value);
+
+      case 'date':
+        return _.isDate(value);
+
+      case 'regexp':
+        return _.isRegExp(value);
+
+      default:
+        console.error('Error: unknown param type: ' + type);
+        return false;
     }
   };
 
