26b122f5539317efea777fe2e25f9461dac8633a
Karim Piyar Ali <karim.piyarali@gmail.com>
2015-02-27 01:28:33 -0500

    finished up all unit tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,5 @@
 /*jshint expr: true*/
+/*jshint unused: false*/
 
 /**
  * @file: tests.js
@@ -219,4 +220,137 @@
       expect(paramDef.isValidWith.bind(paramDef, {})).to.throw(TypeError);
     });
   });
+
+  /*
+  *********FUNC*********
+  */
+  describe('func', function() {
+    var func = window.sjs.func;
+
+    it('Parameter names in error messages', function() {
+      var testFunction = sjs.func({param1: 'string'}, function(param1) {});
+      expect(testFunction.bind(null, 6)).to.throw(TypeError, /param1/);      
+    });
+
+    it('pass in ParamDefinition properties', function() {
+      var testFunction = sjs.func({
+        param1: {
+          types: ["string", "number", Error],
+          allowNull: true
+        }
+      }, function(param1) {});
+
+      expect(testFunction.bind(null, null)).to.not.throw();
+      expect(testFunction.bind(null, 'test')).to.not.throw();
+      expect(testFunction.bind(null, 9)).to.not.throw();
+      expect(testFunction.bind(null, new TypeError())).to.not.throw();
+
+      //now array
+      testFunction = sjs.func([
+        {
+          types: ['string', 'number', Error],
+          allowNull: true
+        }
+      ], function(param1) {});
+
+      expect(testFunction.bind(null, null)).to.not.throw();
+      expect(testFunction.bind(null, 'test')).to.not.throw();
+      expect(testFunction.bind(null, 9)).to.not.throw();
+      expect(testFunction.bind(null, new TypeError())).to.not.throw();
+    });
+
+    it('pass in string types', function() {
+      var testFunction = sjs.func({
+        param1: ['string', 'number']
+      }, function(param1) {});
+
+      expect(testFunction.bind(null, 'test')).to.not.throw();
+      expect(testFunction.bind(null, 9)).to.not.throw();
+
+      //now array
+      testFunction = sjs.func([['string', 'number']], function(param1) {});
+
+      expect(testFunction.bind(null, 'test')).to.not.throw();
+      expect(testFunction.bind(null, 9)).to.not.throw(TypeError);
+    });
+
+    it('pass in custom objects', function() {
+      var testFunction = sjs.func({
+        param1: Error
+      }, function(param1) {});
+
+      expect(testFunction.bind(null, new TypeError())).to.not.throw();
+      expect(testFunction.bind(null, 9)).to.throw(TypeError);
+
+      //now array
+      testFunction = sjs.func([Error], function(param1) {});
+
+      expect(testFunction.bind(null, new TypeError())).to.not.throw();
+      expect(testFunction.bind(null, 9)).to.throw(TypeError);
+    });
+
+    it('pass in ParamDefinition objects', function() {
+      var paramDef = new sjs.ParamDefinition();
+      paramDef.allowNull = true;
+      paramDef.types = 'string';
+
+      var testFunction = sjs.func({param1: paramDef}, function(param1) {});
+
+      expect(testFunction.bind(null, '')).to.not.throw();
+      expect(testFunction.bind(null, null)).to.not.throw();
+      expect(testFunction.bind(null, 6)).to.throw(TypeError);
+
+      //now array
+      testFunction = sjs.func([paramDef], function(param1) {});
+
+      expect(testFunction.bind(null, '')).to.not.throw();
+      expect(testFunction.bind(null, null)).to.not.throw();
+      expect(testFunction.bind(null, 6)).to.throw(TypeError);
+    });
+
+    it('multiple args', function() {
+      var testFunction = sjs.func({
+        param1: 'string',
+        param2: Error,
+        param3: 'number'
+      }, function(param1, param2, param3) {});
+
+      expect(testFunction.bind(null, '', new TypeError(), '')).to.throw(TypeError, /param3/);
+
+      //now array
+      testFunction = sjs.func(['string', Error, 'number'], function(param1) {});
+      expect(testFunction.bind(null, '', new TypeError(), '')).to.throw(TypeError, /parameter 2/);
+    });
+
+    it('pass all four objects', function() {
+      var paramDef = new sjs.ParamDefinition();
+      paramDef.allowNull = true;
+      paramDef.types = 'string';
+
+      var testFunction = sjs.func({
+        param1: {
+          types: 'string',
+          allowUndefined: true
+        },
+        param2: 'string',
+        param3: Error,
+        param4: paramDef
+      }, function(param1, param2, param3, param4) {});
+
+      expect(testFunction.bind(null, undefined, '', new TypeError(), null)).to.not.throw();
+
+      //now array
+      testFunction = sjs.func([{
+          types: 'string',
+          allowUndefined: true
+        },
+        'string',
+        Error,
+        paramDef
+      ], function(param1) {});
+
+      expect(testFunction.bind(null, undefined, '', new TypeError(), null)).to.not.throw();
+    });
+  });
+
 })();
